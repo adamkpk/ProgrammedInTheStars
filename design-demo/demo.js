e79f43aa2a3c749bf6204679140381e41d4fac15
adamkpk <ik1346@my.yorku.ca>
2022-10-30 16:06:03 -0400

    updated design demo

--- design-demo/demo.js
+++ design-demo/demo.js
@@ -1,6 +1,24 @@
-function api() {
-    var sign = document.getElementById("sign").value.toLowerCase();
+var sign;
+
+function data() {
+    sign = document.getElementById("sign").value.toLowerCase();
     console.log(sign);
+
+    document.getElementById("yoursign").style.display = "none";
+    document.getElementById("get").style.display = "none";
+
+    let columns = document.getElementsByClassName("column");
+
+    for (let i = 0; i < columns.length; i++) {
+        columns[i].style.backgroundColor = "rgb(247, 232, 248)";
+    }
+
+    document.getElementById("mydata").innerHTML = "<h1>" + sign + "<br><br></h1>" + sign + " database data here";
+    document.getElementById("getapi").style.display = "inline";
+}
+
+function api() {
+
     const URL = `https://aztro.sameerkumar.website/?sign=${sign}&day=today`;
     fetch(URL, {
         method: 'POST'
@@ -11,22 +29,11 @@ function api() {
         console.log(results);
 
         delete results["date_range"];
-        document.getElementById("result").innerHTML = "";
+        document.getElementById("apidata").innerHTML = "";
         for(i in results) {
             fixedi = i.replaceAll("_", " ");
-            document.getElementById("result").innerHTML += "<b>" + fixedi + ":</b>  <i>" + results[i] + "</i><br><br>";
-        }
-
-        document.getElementById("yoursign").style.display = "none";
-        document.getElementById("get").style.display = "none";
-
-        let columns = document.getElementsByClassName("column");
-
-        for (let i = 0; i < columns.length; i++) {
-            columns[i].style.backgroundColor = "rgb(247, 232, 248)";
-          }
-
-        
+            document.getElementById("apidata").innerHTML += "<b>" + fixedi + ":</b>  <i>" + results[i] + "</i><br><br>";
+        }   
     });
     
 }
