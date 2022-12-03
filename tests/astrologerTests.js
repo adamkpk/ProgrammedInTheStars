c70e2207a3268a9e1c60244c537f207ab6886bec
adamkpk <ik1346@my.yorku.ca>
2022-12-03 09:48:29 -0500

    Formatting edits

--- tests/astrologerTests.js
+++ tests/astrologerTests.js
@@ -1,10 +1,9 @@
 let assert = chai.assert;
 
 describe("Test 0: testing the working of the testrunner", function(){
-    var result = testing();
 
     it("should return 'hello' from the test function in the webapp .js file", function(){
-        assert.equal(result,"hello");
+        assert.equal(testing(),"hello");
     })
 })
 
@@ -16,9 +15,9 @@ describe("Test 1: the 'sign' global variable is not predefined", function(){
 })
 
 describe("Test 2: The bdayToSign() function always returns a valid sign value for month and day inputs within the valid input space (random month + day testing)", function(){
-    var signs = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"];
     
     it("Testing for 2000 random month + day combinations", function(){
+        var signs = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"];
         for(let i = 0; i < 2000; i++) {
             let month = Math.floor(Math.random() * 12);
             let day = Math.ceil(Math.random() * 31);
